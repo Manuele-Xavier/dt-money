fd779d24ffc57b4c022e4af9eda352a00704e1cf
Manuele-Xavier <manu.mouraamd@gmail.com>
2022-04-25 20:58:53 -0300

    finalizing modal and forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
+import { useState } from 'react';
+import Modal from 'react-modal'
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
+    const [isNewTransactionModalOpen, setisNewTransactionModalOpen] = useState(false);
 
-  return (
-    <>
-       <Header/>
-       <Dashboard/>
-       <GlobalStyle/>
-    </>
-  );
+    function handleOpenNewTransactionModalOpen(){
+        setisNewTransactionModalOpen(true);
+    }
+    function handleCloseNewTransactionModalOpen(){
+        setisNewTransactionModalOpen(false);
+
+    }
+
+    return (
+        <>
+            <Header onOpenNewTransactionModal={handleOpenNewTransactionModalOpen}/>
+            <Dashboard />
+            <NewTransactionModal 
+                isOpen={isNewTransactionModalOpen}
+                onRequestClose={handleCloseNewTransactionModalOpen}
+            />
+            <GlobalStyle />
+        </>
+    );
 }
 
 
--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,23 @@
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
+interface Transaction{
+    id:number;
+    title:string;
+    amount:number;
+    type:string;
+    category:string;
+    createdAt: string;
+}
+
 export function TransactionsTable(){
+    //começando com um array vazio
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
+    useEffect(()=>{
+        api.get('transactions')
+        .then(response => setTransactions(response.data.transactions))
+    },[])
     return(
         <Container>
             <table>
@@ -13,18 +30,19 @@ export function TransactionsTable(){
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Desenvolvimento de website</td>
-                        <td className="deposit">R$ 12.000</td>
-                        <td>Desenvolvimento</td>
-                        <td>24/04/2022</td>
-                    </tr>
-                    <tr>
-                        <td>Aluguel</td>
-                        <td className="withdraw">R$ 1.100</td>
-                        <td>Categoria</td>
-                        <td>24/04/2022</td>
-                    </tr>
+                    {transactions.map(transaction=>(
+                            <tr key={transaction.id}>
+                                <td>{transaction.title}</td>
+                                <td className={transaction.type}>{new Intl.NumberFormat('pt-BR',{
+                                    style: 'currency',
+                                    currency: 'BRL'
+                                }).format(transaction.amount)}</td>
+                                <td>{transaction.category}</td>
+                                <td>{new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.createdAt))}</td>
+                            </tr>
+                      
+                    ))}
+                    
                 </tbody>
             </table>
         </Container>
